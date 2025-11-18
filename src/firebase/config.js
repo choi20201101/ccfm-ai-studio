28083522c62088b056f6a022e35edfc56deb54c6
Choi Jaemyung <ceo@mkm20201101.com>
2025-11-18 11:41:58 +0900

    Fix Firebase API key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,9 +3,9 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
-// Step 2에서 받은 Firebase 설정
+
 const firebaseConfig = {
-  apiKey: "AIzaSyClAU5v73d1XNa2vTueat3lLYAaaIyV8Eg",
+  apiKey: "AIzaSyClAU5v7361XNa2vTueat31LYAaaIyV8Eg",
   authDomain: "ccfm-ai-studio.firebaseapp.com",
   projectId: "ccfm-ai-studio",
   storageBucket: "ccfm-ai-studio.firebasestorage.app",
